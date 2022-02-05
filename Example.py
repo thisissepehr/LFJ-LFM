bfb00992e77911436b8d0e73b0e27dac5f20f959
Mojtaba <83760483+MojtabaFarsian@users.noreply.github.com>
2022-02-05 19:43:47 +0330

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,19 +1,29 @@
-from manager import *
+from Manager import *
 
 user = Manager()
 
-user.addMachine()
-user.addMachine()
-user.addMachine()
+# Insert number of machine
+machine_num = 3
 
-user.addTask(1, [0, 1, 2])
-user.addTask(1, [0, 1])
-user.addTask(1, [2])
-user.addTask(1, [0, 1, 2])
-user.addTask(1, [1])
+for i in range(machine_num):
+    user.addMachine()
+
+# Insert Job i   user.addTask(time, [list of Machine can proccess job i])
+# Note: The number of machines and jobs is one less. Be careful when entering it.
+user.addTask(5, [0, 2])
+user.addTask(3, [1, 2])
+user.addTask(7, [0, 1, 2])
+user.addTask(6, [0, 1, 2])
+user.addTask(3, [2])
+
+
 
 user.printTasks()
+print("\n")
 user.printMachines()
 
-user.passTime(13)
+user.passTime(6000)
+
+print("\n")
+print("Result is:")
 user.report()
